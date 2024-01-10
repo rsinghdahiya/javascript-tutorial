33a73ea557960a1896dad82976237dbceebb8f42
rsinghdahiya <156165380+rsinghdahiya@users.noreply.github.com>
2024-01-10 07:54:16 +0000

    Datatype conversion

---
+++ tutorial/01_datatype_conversion.js
@@ -0,0 +1,22 @@
+let myscore = "44abc"
+
+console.log(typeof myscore)
+
+let numberscore = Number(myscore);
+
+console.log(typeof numberscore)
+console.log(numberscore)
+
+// "44" => 44; 
+// "44abc" => NaN;
+// "true" => 1; "false" => 0
+
+let isLogin = "Hello";
+let booleanisLogin = Boolean(isLogin);
+
+console.log(booleanisLogin);
+
+// "1" => true; "0" => false;
+// "" => false;
+//  "Hello" => true;
+
